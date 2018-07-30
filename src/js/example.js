d337bec3e240a40def204ac329ff9565115811ae
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2018-07-30 16:51:24 +0200

    New example code

--- src/js/example.js
+++ src/js/example.js
@@ -1,1 +1,14 @@
-var one = 1;
+// very bad
+const original = { a: 1, b: 2 };
+const copy = Object.assign(original, { c: 3 }); // this mutates `original` ಠ_ಠ
+delete copy.a; // so does this
+
+// bad
+const original = { a: 1, b: 2 };
+const copy = Object.assign({}, original, { c: 3 }); // copy => { a: 1, b: 2, c: 3 }
+
+// good
+const original = { a: 1, b: 2 };
+const copy = { ...original, c: 3 }; // copy => { a: 1, b: 2, c: 3 }
+
+const { a, ...noA } = copy; // noA => { b: 2, c: 3 }
