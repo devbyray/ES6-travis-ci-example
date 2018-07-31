818a8e7d62bcc2b3dd984669f0e4e8e5b602822b
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2018-07-31 20:33:01 +0200

    Fixed linting errors

--- src/js/example.js
+++ src/js/example.js
@@ -1,9 +1,8 @@
 // good
-var b = 'jaja';
-const original = { a: 1, b: 2 };
-const copy = { ...original, c: 3 }; // copy => { a: 1, b: 2, c: 3 }
+function getValues() {
+  const original = { a: 1, b: 2 };
+  const copy = { ...original, c: 3 }; // copy => { a: 1, b: 2, c: 3 }
+  return copy;
+}
 
-const { a, ...noA } = copy; // noA => { b: 2, c: 3 }
-
-console.log('a: ', a);
-console.log('noA: ', noA);
+getValues();
