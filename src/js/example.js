7ab034d33f90f18f0faa1e25e413809497184bab
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2018-07-31 17:02:17 +0200

    Gulp tasks in sperate folders

--- src/js/example.js
+++ src/js/example.js
@@ -1,14 +1,9 @@
-// very bad
-const original = { a: 1, b: 2 };
-const copy = Object.assign(original, { c: 3 }); // this mutates `original` ಠ_ಠ
-delete copy.a; // so does this
-
-// bad
-const original = { a: 1, b: 2 };
-const copy = Object.assign({}, original, { c: 3 }); // copy => { a: 1, b: 2, c: 3 }
-
 // good
+var b = 'jaja';
 const original = { a: 1, b: 2 };
 const copy = { ...original, c: 3 }; // copy => { a: 1, b: 2, c: 3 }
 
 const { a, ...noA } = copy; // noA => { b: 2, c: 3 }
+
+console.log('a: ', a);
+console.log('noA: ', noA);
