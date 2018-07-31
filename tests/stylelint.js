49e583c615b9d3aa99926421963c5ed4e28ceefc
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2018-07-31 20:08:33 +0200

    Added styleint

--- tests/stylelint.js
+++ tests/stylelint.js
@@ -1,15 +1,12 @@
 const gulp = require('gulp');
-const eslint  = require('gulp-eslint');
+const gulpStylelint = require('gulp-stylelint');
 
 module.exports = function() {
-	return gulp.src('src/js/**/*.js', { sourcemaps: true })
-        // eslint() attaches the lint output to the "eslint" property
-        // of the file object so it can be used by other modules.
-        .pipe(eslint())
-        // eslint.format() outputs the lint results to the console.
-        // Alternatively use eslint.formatEach() (see Docs).
-        .pipe(eslint.format())
-        // To have the process exit with an error code (1) on
-        // lint error, return the stream and pipe to failAfterError last.
-        .pipe(eslint.failAfterError());
+	return gulp
+		.src('src/**/*.scss')
+		.pipe(gulpStylelint({
+			reporters: [
+				{formatter: 'string', console: true}
+			]
+		}));
 }
