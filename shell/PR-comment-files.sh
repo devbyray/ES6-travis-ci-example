71f82d5671f6941b7c2ee7f7dacb080a7a96b155
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2018-08-01 22:40:57 +0200

    Debug shell script

--- shell/PR-comment-files.sh
+++ shell/PR-comment-files.sh
@@ -2,6 +2,8 @@
 set -ev
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
    # hey that's a pull request
+   echo "Comment: ${PR_COMMENT}";
+
    curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
     -d "{\"body\": \"${PR_COMMENT}\"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
