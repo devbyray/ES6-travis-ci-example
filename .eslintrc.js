965dbd2cda91db76010b993cd3609231bfe0f530
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2018-08-16 10:56:39 +0200

    Setup ESLINT with StandardJS config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,15 @@
 module.exports = {
-    "extends": ["eslint:recommended", "airbnb"],
+    "extends": ["standard"],
     "env": {
         "browser": true,
-        "es6": true
+        "es6": true,
+        "node": true
     },
     "parserOptions": {
         "ecmaVersion": 2018,
         "sourceType": "module"
     },
+    "rules": {
+        "no-console": "warn"
+    }
 };
