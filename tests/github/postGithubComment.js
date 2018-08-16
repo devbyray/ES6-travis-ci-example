f76c3882c3e32c4cdfcc89ac0623ff1506429040
Raymon Schouwenaar <raymon@raymonschouwenaar.nl>
2018-08-16 11:35:20 +0200

    Better check for PR ID postGithubComment

--- tests/github/postGithubComment.js
+++ tests/github/postGithubComment.js
@@ -3,7 +3,7 @@ const AXIOS = require('axios')
 AXIOS.defaults.headers.post['Content-Type'] = SETTINGS.DEFAULT_AXIOS_CONTENT_TYPE
 
 module.exports = function (comment, repoSlug, prId, ghToken) {
-  if (prId) {
+  if (typeof prId === 'number' && prId >= 0) {
     return AXIOS.post(`https://api.github.com/repos/${repoSlug}/issues/${prId}/comments?access_token=${ghToken}`, {
       body: comment
     })
